16615485dcee160c9160899029b9a30ed48c9d2f
Tasikul Islam <iamtasikul@gmail.com>
2020-12-13 20:39:00 +0530

    Showing Article in HomePage

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const app = express();
 app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
 app.get("/", (req, res) => {
-  //   res.send("Hello hi");
-  res.render("index");
+  const articles1 = [
+    {
+      title: "Test Articles",
+      createdAt: new Date(),
+      description: "Test Description",
+    },
+    {
+      title: "Test Articles 2",
+      createdAt: new Date(),
+      description: "Test Description 2",
+    },
+  ];
+  res.render("index", { articles: articles1 });
 });
 
 app.listen(5000);
